484fc001c9d289d6ce0f1b50f21d0809bb11039a
IchalAlwiros <alwiros99@gmail.com>
2022-12-22 10:39:25 +0700

    add multer router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config()
 
+// Inital Router
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,8 +22,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use Router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Setup Static File
+// Bertujuan untuk menampilkan sebuah file
+app.use('/assets',express.static('public/images'))
+
+// Middleware Multer
+const upload = require('./middleware/multer')
+app.post('/upload',upload.single('photo'), (req, res)=>{
+    return res.json({
+        message : 'upload success'
+    })
+} )
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +51,12 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500).json({
+    error: {
+      status : err.status, 
+      message : err.message
+    }
+  });
   res.render('error');
 });
 
